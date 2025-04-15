1549bb50bd0058a961c4c4dc20b100a1ba41c11b
Marieza20 <mariezact@hotmail.com>
2025-04-15 10:12:42 -0600

    Avance Quiz

--- Juego.py
+++ Juego.py
@@ -2,12 +2,26 @@ class Juego:
     def __init__(self, nombreJ):
         self.nombreJ = nombreJ
         self.jugando = False
+        self.abecedario = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
     
     def iniciar(self):
         self.jugando = True
         print(f"Bienvenido(a) {self.nombreJ}, el juego ha comenzado.")
-        
+        Juego.desplazar(self)
         print("¡Gracias por jugar!")
         
     def salir(self):
         print("Saliendo del juego...")
+        
+    def desplazar(self):
+        letras = input("Igrese la palabra a encriptar: ")
+        for letra in letras:
+            letraMayuscula = letra.upper()
+            if letraMayuscula in self.abecedario:
+                posicion = self.abecedario.index(letraMayuscula)
+                nuevaPosicion = posicion + 1
+                if nuevaPosicion >= len(self.abecedario):
+                    nuevaPosicion = 0
+                letraEncriptada = self.abecedario[nuevaPosicion]
+                print(letraEncriptada, end="")
+        print("")
